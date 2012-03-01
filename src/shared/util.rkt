7906c47ae722df9f3b8bc34b701165f08d561e7e
Eliott Wiener <snuffy@breakout.ccs.neu.edu>
2012-03-01 13:25:58 -0500

    Added is-mp3? procedure

--- src/shared/util.rkt
+++ src/shared/util.rkt
@@ -9,3 +9,11 @@
             (check 9 #\A)
             (check 10 #\V)
             (check 11 #\E))))
+
+;Given a byte string bs, determine if bs represents a .mp3 file
+;NOTE: This is a pretty simplistic checking method. It only works for MP3s with ID3 metadata tags.
+(define (is-mp3? bs)
+    (let ((check (lambda (index value) (= (bytes-ref bs index) (char->integer value)))))
+        (and (check 0 #\I)
+            (check 1 #\D)
+            (check 2 #\3))))
