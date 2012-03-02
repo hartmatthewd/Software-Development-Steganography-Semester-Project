6ad2993b8a10dc33406df00139f6b13ffb548c93
hartmatthewd <hart.matthewd@gmail.com>
2012-03-02 15:22:33 -0500

    Added functions to read file into a wavfile struct and also to write back to a bytestring. NOTE that there is a bug somewhere currently

--- src/shared/util.rkt
+++ src/shared/util.rkt
@@ -19,31 +19,80 @@
             (check 2 #\3))))
 
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;                                         ;;;;;;;;;
+;;;;;;;;     Reading and writing of WAV files    ;;;;;;;;;
+;;;;;;;;                                         ;;;;;;;;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+;;; A holder for all the data needed to recreate the wav file
+(struct wavfile (endianess audioformat channels samplerate byterate blockalign bitspersample chunkstart chunksize samples))
+
+
+;;; Writes *destructively* the given vector of samples to the bytes
+;;; bytes - a bytestring to write to
+;;; wav - the wavfile to retrieve the samples from
+;;; channel - the channel to write to
+(define (write-wavfile-bytes-for-channel bytes wav channel)
+    (letrec ((samples (vector-ref (wavfile-samples wav) 
+                                  channel))
+             (get-first-byte (if (equal? (wavfile-endianess wav) 'little)
+                                 (lambda (sample) (modulo sample 256))
+                                 (lambda (sample) (floor (/ sample 256)))))
+             (get-second-byte (if (equal? (wavfile-endianess wav) 'little)
+                                  (lambda (sample) (floor (/ sample 256)))
+                                  (lambda (sample) (modulo sample 256))))
+             (set-next-byte (lambda (byte sample)
+                                    (when (not (= sample (vector-length samples)))
+                                          (begin (bytes-set! bytes byte       (get-first-byte (vector-ref samples sample)))
+                                                 (bytes-set! bytes (+ byte 1) (get-second-byte (vector-ref samples sample)))
+                                                 (set-next-byte (+ byte (* 2 (wavfile-channels wav))) (+ sample 1)))))))
+            (set-next-byte (+ (wavfile-chunkstart wav) (* 2 channel)) 0)))
+
+
+
 ;;; Returns a vector representing the samples of either the left or right channel
 ;;; bytes - a bytestring representing wav file data
-;;; start - the starting byte of the data segment
-;;; channels - the number of channels in the wav file
+;;; wav - the wavfile to write to
 ;;; channel - the channel to convert
-;;; endianess - the endianness of the wav data
 ;;; TODO - this assumes 2 byte samples, maybe change this
 
-(define (get-sample-vector-from-bytes bytes start channels channel endianess)
-    (letrec ((samples (/ (- (bytes-length bytes) start) (* 2 channels)))
-             (vector (make-vector samples))
-             (get-sample-value (if (equal? endianess 'little)
+(define (set-wavfile-samples-for-channel bytes wav channel)
+    (letrec ((samples (vector-ref (wavfile-samples wav)
+                                  channel))
+             (get-sample-value (if (equal? (wavfile-endianess wav) 'little)
                                    (lambda (a b) (+ a (* 256 b)))
                                    (lambda (a b) (+ (* 256 a) b))))
              (set-next-sample (lambda (byte sample)
-                                      (if (= sample samples)
-                                          vector
-                                          (begin (vector-set! vector sample (get-sample-value (bytes-ref bytes byte) (bytes-ref bytes (+ byte 1))))
-                                                 (set-next-sample (+ byte (* 2 channels)) (+ sample 1)))))))
-            (set-next-sample (+ start channel) 0)))
+                                      (when (not (= sample (vector-length samples)))
+                                            (begin (vector-set! samples 
+                                                                sample 
+                                                                (get-sample-value (bytes-ref bytes byte) 
+                                                                                  (bytes-ref bytes (+ byte 1))))
+                                                   (set-next-sample (+ byte (* 2 (wavfile-channels wav))) (+ sample 1)))))))
+            (set-next-sample (+ (wavfile-chunkstart wav) (* 2 channel)) 0)))
 
-;;; A holder for all the data needed to recreate the wav file
-(struct wavfile (endianess audioformat channels samplerate byterate blockalign bitspersample chunkstart chunksize lsamples rsamples) #:mutable)
 
-;;; Given a file, create a wavfile structi
+
+;;; Given a bytestring and a wavfile struct, fill in the samples for each channel
+(define (set-wavfile-samples bytes wav)
+    (letrec ((set-next-channel (lambda (channel)
+                                       (when (not (= channel (wavfile-channels wav)))
+                                             (begin (set-wavfile-samples-for-channel bytes wav channel)
+                                                    (set-next-channel (+ 1 channel)))))))
+            (set-next-channel 0)))
+
+;;; Given a bytestring and a wavfile struct, rewrite *destructively* the wav samples to the bytestring
+(define (write-wavfile-bytes bytes wav)
+    (letrec ((set-next-channel (lambda (channel)
+                                       (when (not (= channel (wavfile-channels wav)))
+                                             (begin (write-wavfile-bytes-for-channel bytes wav channel)
+                                                    (set-next-channel (+ 1 channel)))))))
+            (set-next-channel 0)))
+
+
+;;; Given a file, create a wavfile struct
 ;;; Requires - shared/fileio.rkt, shared/wav.sps
 ;;; TODO - when not wav, convert to wav
 (define (make-wavfile file)
@@ -51,16 +100,7 @@
          (if (is-wav? bytes)
              (let ((wav (call-with-values (lambda () (parse-wave-header bytes))
                                           (lambda (e af c sr by ba bps s cs) 
-                                                  (wavfile e af c sr by ba bps s cs null null)))))
-                  (set-wavfile-lsamples! wav (get-sample-vector-from-bytes bytes (wavfile-chunkstart wav) 
-                                                                                 (wavfile-channels wav) 
-                                                                                 0 
-                                                                                 (wavfile-endianess wav)))
-                  (if (= 2 (wavfile-channels wav))
-                      (set-wavfile-lsamples! wav (get-sample-vector-from-bytes bytes (wavfile-chunkstart wav) 
-                                                                                     (wavfile-channels wav) 
-                                                                                     1 
-                                                                                     (wavfile-endianess wav)))
-                      '())
+                                                  (wavfile e af c sr by ba bps s (- cs s) (make-vector c (make-vector (/ (- cs s) (* 2 c)) 0)))))))
+                  (set-wavfile-samples bytes wav)
                   wav)
              (error "File is not a wav file"))))
