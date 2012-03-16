a84cc46250e5433ba8ceb6bbcabdf8d69af55317
Eliott Wiener <snuffy@armorattack.ccs.neu.edu>
2012-03-16 12:23:08 -0400

    Re-added tests for 'is-mp3?' procedure.

--- tst/fileio.rkt
+++ tst/fileio.rkt
@@ -10,10 +10,8 @@
    (test-set-wavfile-samples))
 
 (define (test-is-mp3?)
-   (display "WARNING: is-mp3? is too simplified and not working for all mp3s. Ignoring test for now.")
-   ;(check-false (is-mp3? "testwav.wav"))
-   ;(check-true (is-mp3? "testmp3.mp3")))
-)
+   (check-false (is-mp3? "testwav.wav"))
+   (check-true (is-mp3? "testmp3.mp3")))
 
 (define (test-is-wav?)
    (check-true (is-wav? "testwav.wav"))
