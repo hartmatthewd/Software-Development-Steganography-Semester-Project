dfb21e2528213cb9797482c56b9ba76fcf9a5cc8
hartmatthewd <hart.matthewd@gmail.com>
2012-04-03 11:03:43 -0400

    added some tests for fileio

--- tst/fileio.rkt
+++ tst/fileio.rkt
@@ -1,9 +1,11 @@
+;;;;;;;;;;;;;;;;;;
 (define (run-fileio-tests)
    (load "src/fileio.rkt")
 
    (open-file-input-port-test)
    (open-file-output-port-test)
    (read-bytes-from-file-test)
+   (write-bytes-to-file-test)
    (pipe-remaining-bytes-test)
    (read-file-into-bytestring-test)
    (write-bytestring-to-file-test)
@@ -12,78 +14,80 @@
    (is-wav?-test)
    (is-mp3?-test)
    (wav->mp3-test)
-   (mp3->wav-test)
-   (test-file-writer-reader))
+   (mp3->wav-test))
 
+;;;;;;;;;;;;;;;;;;
 (define (open-file-input-port-test)
    ;(open-file-input-port file)
    (display "No test cases for open-file-input-port\n"))
 
+;;;;;;;;;;;;;;;;;;
 (define (open-file-output-port-test)
    ;(open-file-output-port file)
    (display "No test cases for open-file-output-port\n"))
 
+;;;;;;;;;;;;;;;;;;
 (define (read-bytes-from-file-test)
    ;(read-bytes-from-file amt in-port)
    (display "No test cases for read-bytes-from-file\n"))
 
+;;;;;;;;;;;;;;;;;;
+(define (read-file-into-bytestring-test)
+   ;(write-bytes-to-file bytes out-port)
+   (display "No test cases for write-bytes-to-file\n"))
+
+;;;;;;;;;;;;;;;;;;
 (define (write-bytes-to-file-test)
    ;(write-bytes-to-file bytes out-port)
    (display "No test cases for write-bytes-to-file\n"))
 
+;;;;;;;;;;;;;;;;;;
 (define (pipe-remaining-bytes-test)
-   ;(pipe-remaining-bytes in out)
-   (display "No test cases for pipe-remaining-bytes\n"))
+   (copy-file "testwav.wav" tmpsrc #t)
+   (let [(in (open-file-input-port tmpsrc))
+         (out (open-file-output-port tmpdest))]
+        (pipe-remaining-bytes in out)
+        (finalize-input-port in)
+        (finalize-output-port out))
+   (check-equal? (file->bytes tmpsrc) (file->bytes tmpdest)))
 
+
+;;;;;;;;;;;;;;;;;;
 (define (read-file-into-bytestring-test)
    ;(read-file-into-bytestring file)
    (display "No test cases for read-file-into-bytestring\n"))
 
+;;;;;;;;;;;;;;;;;;
 (define (write-bytestring-to-file-test)
    ;(write-bytestring-to-file bytestring file)
    (display "No test cases for write-bytestring-to-file\n"))
 
+;;;;;;;;;;;;;;;;;;
 (define (finalize-input-port-test)
    ;(finalize-input-port input)
    (display "No test cases for finalize-input-port\n"))
 
+;;;;;;;;;;;;;;;;;;
 (define (finalize-output-port-test)
    ;(finalize-output-port output)
    (display "No test cases for finalize-output-port\n"))
 
+;;;;;;;;;;;;;;;;;;
 (define (is-wav?-test)
-   ;(is-wav? file)
-   (display "No test cases for is-wav?\n"))
-
-(define (is-mp3?-test)
-   ;(is-mp3? file)
-   (check-false (is-mp3? "testwav.wav"))
-   (check-true (is-mp3? "testmp3.mp3")))
-
-(define (wav->mp3-test)
-   ;(wav->mp3 wav mp3)
    (check-true (is-wav? "testwav.wav"))
    (check-false (is-wav? "testmp3.mp3")))
 
-(define (mp3->wav-test)
-   ;(mp3->wav mp3 wav)
-   (display "No test cases for mp3->wav\n"))
+;;;;;;;;;;;;;;;;;;
+(define (is-mp3?-test)
+   (check-false (is-mp3? "testwav.wav"))
+   (check-true (is-mp3? "testmp3.mp3")))
 
-(define (test-file-writer-reader)
-   (display "No test cases for test-file-writer-reader\n"))
-   ; Fileio should have no knowledge of wavfiles therefor they cannot be used for testing
-   ;    (let [(sample-wavfile (create-test-wavfile))]
-   ;         (vector-set! (vector-ref (wavfile-samples sample-wavfile) 0) 0 -44)
-   ;         (vector-set! (vector-ref (wavfile-samples sample-wavfile) 0) 1 13533)
-   ;         (wavfile->file sample-wavfile "/tmp/_test")
-   ;         (let [(w (file->wavfile "/tmp/_test"))]
-   ;              (check-equal? (wavfile-endianess sample-wavfile) (wavfile-endianess w))
-   ;              (check-equal? (wavfile-audioformat sample-wavfile) (wavfile-audioformat w))
-   ;              (check-equal? (wavfile-channels sample-wavfile) (wavfile-channels w))
-   ;              (check-equal? (wavfile-samplerate sample-wavfile) (wavfile-samplerate w))
-   ;              (check-equal? (wavfile-byterate sample-wavfile) (wavfile-byterate w))
-   ;              (check-equal? (wavfile-blockalign sample-wavfile) (wavfile-blockalign w))
-   ;              (check-equal? (wavfile-bytespersample sample-wavfile) (wavfile-bytespersample w))
-   ;              (check-equal? (wavfile-chunkstart sample-wavfile) (wavfile-chunkstart w))
-   ;              (check-equal? (wavfile-chunksize sample-wavfile) (wavfile-chunksize w))
-   ;              (check-equal? (wavfile-samples sample-wavfile) (wavfile-samples w)))))
+;;;;;;;;;;;;;;;;;;
+(define (wav->mp3-test)
+   (wav->mp3 "testwav.wav" tmpsrc)
+   (check-true (is-mp3? tmpsrc)))
+
+;;;;;;;;;;;;;;;;;;
+(define (mp3->wav-test)
+   (mp3->wav "testmp3.mp3" tmpsrc)
+   (check-true (is-wav? tmpsrc)))
